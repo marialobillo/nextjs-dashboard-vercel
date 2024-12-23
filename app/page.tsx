bb6f8e4b39ce4d9a4fcc9371f7fdd5ce007f258a
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-12-23 18:38:16 +0100

    feat: add 2 images for diff sizes

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/font';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape}>
+      {/* <div className={styles.shape}>
 
-      </div>
+      </div> */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
@@ -35,7 +36,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            alt="Screenshorts of the dashboard project showing desktop version"
+            className="hidden md:block"
+            width={1000}
+            height={760}
+          />
+          <Image
+            src="/hero-mobile.png"
+            alt="Screenshorts of the dashboard project showing mobile version"
+            className="block md:hidden"
+            width={375}
+            height={667}
+          />
         </div>
       </div>
     </main>
